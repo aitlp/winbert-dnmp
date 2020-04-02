6c5a4083558b4df49755d79813a1e076ee5f3cd5
aitlp <aitlpmw@gmail.com>
2020-04-02 08:02:17 +0800

    add -A

--- README.md
+++ README.md
@@ -9,10 +9,62 @@
 
 将`sample.env`文件，重命名为`.env`，里面配置的数据库密码等信息。
 
-2.修改`init-letsencrypt.sh`
+2.为`4spaces.net`申请证书
 
-将`your_domain`修改为自己的域名，将`your_email_address`修改为自己的邮箱地址。
+1）修改`init-letsencrypt.sh`
 
-3.修改Nginx配置
+将`your_domain`修改为`4spaces.net`，将`your_email_address`修改为自己的邮箱地址。
 
-其中，`services/nginx/conf.d/4spaces.conf`文件为网站域名对应配置；`v2ray.conf`为v2ray对应配置。
+2) 修改Nginx配置
+
+其中，`services/nginx/conf.d/4spaces.conf`文件为网站域名对应配置；`services/nginx/conf.d/v2ray.conf`为v2ray对应配置。
+
+将`services/nginx/conf.d/v2ray.conf`重命名为`services/nginx/conf.d/v2ray.conf.bak`，命令为：
+
+```
+mv services/nginx/conf.d/v2ray.conf  services/nginx/conf.d/v2ray.conf.bak
+```
+
+3) 开始申请证书
+
+```
+sh ./init-letsencrypt.sh
+```
+
+3.为`v2.coding996.com`申请证书
+
+1）修改`init-letsencrypt.sh`
+
+将`your_domain`修改为`v2.coding996.com`，将`your_email_address`修改为自己的邮箱地址。
+
+2) 修改Nginx配置
+
+将`services/nginx/conf.d/4spaces.conf`重命名为`services/nginx/conf.d/4spaces.conf.bak`，命令为：
+
+```
+mv services/nginx/conf.d/4spaces.conf  services/nginx/conf.d/4spaces.conf.bak
+```
+
+3）停止`nginx`
+
+```
+docker container stop nginx
+```
+
+4) 开始申请证书
+
+```
+sh ./init-letsencrypt.sh
+```
+
+4.nginx配置文件复原
+
+将刚才命名为`services/nginx/conf.d/4spaces.conf.bak`改回`services/nginx/conf.d/4spaces.conf`。
+
+5.重启nginx
+
+```
+docker exec nginx nginx -s reload
+```
+
+完成。
--- www/4spaces/index.php
+++ www/4spaces/index.php
@@ -9,8 +9,6 @@ echo '<ul>';
 echo '<li>PHP版本：', PHP_VERSION, '</li>';
 echo '<li>Nginx版本：', $_SERVER['SERVER_SOFTWARE'], '</li>';
 echo '<li>MySQL服务器版本：', getMysqlVersion(), '</li>';
-echo '<li>Redis服务器版本：', getRedisVersion(), '</li>';
-echo '<li>MongoDB服务器版本：', getMongoVersion(), '</li>';
 echo '</ul>';
 
 echo '<h2>已安装扩展</h2>';
@@ -35,46 +33,6 @@ function getMysqlVersion()
         return 'PDO_MYSQL 扩展未安装 ×';
     }
 
-}
-
-/**
- * 获取Redis版本
- */
-function getRedisVersion()
-{
-    if (extension_loaded('redis')) {
-        try {
-            $redis = new Redis();
-            $redis->connect('redis', 6379);
-            $info = $redis->info();
-            return $info['redis_version'];
-        } catch (Exception $e) {
-            return $e->getMessage();
-        }
-    } else {
-        return 'Redis 扩展未安装 ×';
-    }
-}
-
-/**
- * 获取MongoDB版本
- */
-function getMongoVersion()
-{
-    if (extension_loaded('mongodb')) {
-        try {
-            $manager = new MongoDB\Driver\Manager('mongodb://root:123456@mongodb:27017');
-            $command = new MongoDB\Driver\Command(array('serverStatus'=>true));
-
-            $cursor = $manager->executeCommand('admin', $command);
-
-            return $cursor->toArray()[0]->version;
-        } catch (Exception $e) {
-            return $e->getMessage();
-        }
-    } else {
-        return 'MongoDB 扩展未安装 ×';
-    }
 }
 
 /**
